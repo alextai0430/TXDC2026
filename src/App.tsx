00d15cddbd5a90ca93f0508421efb699a1b99575
Alexander Tai <alextai0430@gmail.com>
2025-10-17 12:01:02 -0400

    all

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+      <div className="app">
+        <h1>Texas Diabolo Competition 2026</h1>
+      </div>
+  );
+}
+
+export default App;
